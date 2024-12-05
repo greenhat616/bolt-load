45124e77370d25399886f5a9e08bf5f46c7b9d0f
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-12-05 23:23:20 +0800

    tests: add http no range and range handler server

--- src/adapter/mod.rs
+++ src/adapter/mod.rs
@@ -36,3 +36,106 @@ pub type AnyStream<T> = Box<dyn Stream<Item = T> + Unpin + Send>;
 // TODO: maybe the chunk should be zero copy
 // pub trait BoltLoaderAdapterAnyStream =
 //     BoltLoadAdapter<Box<dyn Stream<Item = Vec<u8>> + Send>, Vec<u8>>;
+
+#[cfg(test)]
+mod tests {
+    use axum::response::IntoResponse;
+    use rand::Rng;
+    use std::{
+        io::{BufWriter, Seek, Write},
+        sync::Arc,
+    };
+    use tempfile::tempfile;
+    use tokio::{io::AsyncSeekExt, net::TcpListener};
+
+    fn create_random_file(size: usize) -> anyhow::Result<std::fs::File> {
+        let mut file = tempfile()?;
+        let mut writer = BufWriter::new(file.try_clone()?);
+        let mut rng = rand::thread_rng();
+        let mut buffer = [0; 1024];
+        let mut remaining_size = size;
+        while remaining_size > 0 {
+            let bytes_to_write = std::cmp::min(remaining_size, buffer.len());
+            rng.fill(&mut buffer[..bytes_to_write]);
+            writer.write_all(&buffer[..bytes_to_write])?;
+            remaining_size -= bytes_to_write;
+        }
+        // reset the file pointer to the beginning
+        file.seek(std::io::SeekFrom::Start(0))?;
+        Ok(file)
+    }
+
+    #[derive(Clone)]
+    struct FileHolder(Arc<tokio::sync::Mutex<tokio::fs::File>>);
+
+    async fn create_http_server() -> anyhow::Result<(u16, tokio::task::JoinHandle<()>)> {
+        use axum::extract::State;
+        use axum_extra::TypedHeader;
+
+        let file = tokio::task::spawn_blocking(|| create_random_file(1024 * 1024)).await??;
+        let holder = FileHolder(Arc::new(tokio::sync::Mutex::new(
+            tokio::fs::File::from_std(file),
+        )));
+        let port = portpicker::pick_unused_port()
+            .ok_or(anyhow::anyhow!("Failed to pick an unused port"))?;
+        let listener = TcpListener::bind(("127.0.0.1", port)).await?;
+
+        /// a handler send without range
+        async fn no_range_handler(
+            State(holder): State<FileHolder>,
+        ) -> impl axum::response::IntoResponse {
+            let mut file = holder.0.lock().await;
+            match file.seek(std::io::SeekFrom::Start(0)).await {
+                Ok(_) => {
+                    let reader = tokio_util::io::ReaderStream::new(file.try_clone().await.unwrap());
+                    let body = axum::body::Body::from_stream(reader);
+                    let headers = [
+                        (
+                            axum::http::header::CONTENT_TYPE,
+                            "text/plain; charset=utf-8",
+                        ),
+                        (
+                            axum::http::header::CONTENT_DISPOSITION,
+                            "attachment; filename=\"test.txt\"",
+                        ),
+                    ];
+                    return (headers, body).into_response();
+                }
+                Err(e) => {
+                    return (
+                        axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                        e.to_string().into_response(),
+                    )
+                        .into_response();
+                }
+            }
+        }
+
+        /// a handler mock range stream
+        async fn range_handler(
+            State(holder): State<FileHolder>,
+            range: Option<TypedHeader<axum_extra::headers::Range>>,
+        ) -> impl axum::response::IntoResponse {
+            let file = holder.0.lock().await;
+            let mut file_cloned = file.try_clone().await.unwrap();
+            file_cloned.seek(std::io::SeekFrom::Start(0)).await.unwrap();
+            let body = axum_range::KnownSize::file(file_cloned).await.unwrap();
+            let range = range.map(|TypedHeader(range)| range);
+            let ranged = axum_range::Ranged::new(range, body);
+            ranged.into_response()
+        }
+
+        let app = axum::Router::new()
+            .route("/no_range", axum::routing::get(no_range_handler))
+            .route("/range", axum::routing::get(range_handler))
+            .with_state(holder);
+
+        let handle = tokio::spawn(async move {
+            axum::serve(listener, app.into_make_service())
+                .await
+                .unwrap()
+        });
+
+        Ok((port, handle))
+    }
+}
