b6564dac8b4294e50ef566bb36c4549bfeee19ed
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2025-06-25 00:47:10 +0800

    fix(test): make file writer work

--- src/task/instance/concurrent_task/file.rs
+++ src/task/instance/concurrent_task/file.rs
@@ -62,14 +62,25 @@ impl FileWriterControl {
             .send(Payload::new(range, data, tx))
             .await
             .map_err(FileWriterError::Send)?;
-        rx.await.map_err(FileWriterError::Recv)?;
+        rx.await
+            .map_err(FileWriterError::Recv)?
+            .map_err(FileWriterError::Io)?;
         Ok(())
     }
+}
+
+#[derive(Default, PartialEq, Eq)]
+#[allow(dead_code)]
+enum Mode {
+    #[default]
+    Mmap,
+    SeekWrite,
 }
 
 pub struct FileWriter {
     total: u64,
     path: PathBuf,
+    mode: Mode,
 }
 
 impl FileWriter {
@@ -77,7 +88,13 @@ impl FileWriter {
         Self {
             total,
             path: path.as_ref().to_path_buf(),
+            mode: Mode::default(),
         }
+    }
+
+    #[allow(unused)]
+    fn set_mode(&mut self, mode: Mode) {
+        self.mode = mode;
     }
 
     fn handle_seek_write(
@@ -93,17 +110,35 @@ impl FileWriter {
     pub fn start(self) -> FileWriterGuard {
         let (tx, rx) = async_channel::bounded(FILE_WRITER_QUEUE_SIZE);
         let total = self.total;
+        let mode = self.mode;
+        log::trace!("start file writer: {:?}", self.path);
         let handle = std::thread::spawn(move || {
-            let meta = std::fs::metadata(&self.path)?;
-            let mut file = OpenOptions::new().write(true).open(&self.path)?;
-            if meta.len() != total {
-                file.set_len(total)?;
+            let file_size = std::fs::metadata(&self.path)
+                .map(|meta| meta.len())
+                .unwrap_or(0);
+
+            log::trace!("file size: {file_size:?}");
+            let mut opts = OpenOptions::new();
+            opts.read(true).write(true).create(true).truncate(false);
+
+            let mut file = opts.open(&self.path).inspect_err(|e| {
+                log::error!("failed to open file: {e:?}");
+            })?;
+            if file_size != total {
+                file.set_len(total).inspect_err(|e| {
+                    log::error!("failed to set file length: {e:?}");
+                })?;
             }
 
             // In 32-bit machine, the pointer size is 4 bytes, some large file may exceed the pointer size
             // so we use the seek write to write the file
-            if meta.len() <= isize::MAX as u64 {
-                let mut mmap = unsafe { MmapMut::map_mut(&file)? };
+            if file_size <= isize::MAX as u64 && mode == Mode::Mmap {
+                log::trace!("use mmap mode");
+                let mut mmap = unsafe {
+                    MmapMut::map_mut(&file).inspect_err(|e| {
+                        log::error!("failed to map file: {e:?}");
+                    })?
+                };
                 while let Ok(Payload(range, data, tx)) = rx.recv_blocking() {
                     mmap[range.start as usize..range.end as usize].copy_from_slice(&data);
                     let _ = tx.send(mmap.flush_async_range(
@@ -112,6 +147,7 @@ impl FileWriter {
                     ));
                 }
             } else {
+                log::trace!("use seek write mode");
                 while let Ok(Payload(range, data, tx)) = rx.recv_blocking() {
                     let _ = tx.send(Self::handle_seek_write(&mut file, range, data));
                 }
@@ -126,9 +162,10 @@ impl FileWriter {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_log::test;
 
-    #[tokio::test]
-    async fn test_file_writer() {
+    #[test(tokio::test)]
+    async fn test_mmap_file_writer() {
         let tmp_file = tempfile::tempdir().unwrap();
         let file_path = tmp_file.path().join("test.txt");
 
@@ -137,12 +174,39 @@ mod tests {
 
         let file_writer = FileWriter::new(&file_path, bytes_len as u64);
         let FileWriterGuard(handle, guard) = file_writer.start();
-        handle.join().unwrap().unwrap();
 
         guard
             .write(0..bytes_len as u64, bytes.clone())
             .await
             .unwrap();
+
+        drop(guard);
+        handle.join().unwrap().unwrap();
+
+        let file = OpenOptions::new().read(true).open(&file_path).unwrap();
+        let mmap = unsafe { memmap2::Mmap::map(&file).unwrap() };
+        assert_eq!(mmap[0..bytes_len], bytes);
+    }
+
+    #[test(tokio::test)]
+    async fn test_seek_write_file_writer() {
+        let tmp_file = tempfile::tempdir().unwrap();
+        let file_path = tmp_file.path().join("test.txt");
+
+        let bytes = Bytes::from_static(b"Hello, world!");
+        let bytes_len = bytes.len();
+
+        let mut file_writer = FileWriter::new(&file_path, bytes_len as u64);
+        file_writer.set_mode(Mode::SeekWrite);
+        let FileWriterGuard(handle, guard) = file_writer.start();
+
+        guard
+            .write(0..bytes_len as u64, bytes.clone())
+            .await
+            .unwrap();
+
+        drop(guard);
+        handle.join().unwrap().unwrap();
 
         let file = OpenOptions::new().read(true).open(&file_path).unwrap();
         let mmap = unsafe { memmap2::Mmap::map(&file).unwrap() };
